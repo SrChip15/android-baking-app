4625652329d4c10f4f1faa0f514f324fb26b7f1c
Sri <k3rn3l16@outlook.com>
2018-06-20 03:06:52 -0700

    Add business objects

---
+++ app/src/main/java/com/android/kakashi/bakingapp/model/Ingredient.java
@@ -0,0 +1,43 @@
+
+package com.android.kakashi.bakingapp.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+@SuppressWarnings("unused")
+public class Ingredient {
+
+    @SerializedName("quantity")
+    @Expose
+    private int quantity;
+    @SerializedName("measure")
+    @Expose
+    private String measure;
+    @SerializedName("ingredient")
+    @Expose
+    private String ingredient;
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getMeasure() {
+        return measure;
+    }
+
+    public void setMeasure(String measure) {
+        this.measure = measure;
+    }
+
+    public String getIngredient() {
+        return ingredient;
+    }
+
+    public void setIngredient(String ingredient) {
+        this.ingredient = ingredient;
+    }
+}
