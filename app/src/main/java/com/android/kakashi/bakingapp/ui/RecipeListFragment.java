2c429eb4e8108ea73636f2a371cc9efad4f67993
Sri <k3rn3l16@outlook.com>
2018-06-24 00:10:48 -0700

    Add FetchRecipesTask to display recipes in splash screen

--- app/src/main/java/com/android/kakashi/bakingapp/ui/RecipeListFragment.java
+++ app/src/main/java/com/android/kakashi/bakingapp/ui/RecipeListFragment.java
@@ -1,6 +1,9 @@
 package com.android.kakashi.bakingapp.ui;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -14,19 +17,33 @@ import android.widget.ListView;
 
 import com.android.kakashi.bakingapp.R;
 import com.android.kakashi.bakingapp.RecipeActivity;
+import com.android.kakashi.bakingapp.data.model.Recipe;
+import com.android.kakashi.bakingapp.data.network.NetworkModule;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class RecipeListFragment extends Fragment {
+
     @BindView(R.id.list_view)
     ListView listView;
+    @SuppressWarnings("FieldCanBeLocal")
+    private NetworkModule networkModule;
 
     public static RecipeListFragment newInstance() {
         // vanilla for now
         return new RecipeListFragment();
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        networkModule = NetworkModule.getInstance();
+        new FetchRecipesTask(networkModule).execute();
     }
 
     @Nullable
@@ -36,35 +53,52 @@ public class RecipeListFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_recipe_list, container, false);
         ButterKnife.bind(this, view);
 
-        // test data
-        final String[] values = new String[20];
-        for (int i = 0; i < values.length; i++) {
-            values[i] = "Recipe Item #" + (i + 1);
-        }
-
-        final ArrayAdapter<String> adapter = new ArrayAdapter<>(
-                Objects.requireNonNull(getActivity()),
-                android.R.layout.simple_list_item_1,
-                android.R.id.text1,
-                values
-        );
-
-        listView.setAdapter(adapter);
-
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                /*Toast.makeText(
-                        getActivity(),
-                        "Item at " + position + " position clicked",
-                        Toast.LENGTH_SHORT
-                ).show();*/
-
-                Intent fireRecipeActivity = RecipeActivity.startActivity(getActivity(), values[position]);
+                Intent fireRecipeActivity = RecipeActivity.startActivity(getActivity(), position);
                 startActivity(fireRecipeActivity);
             }
         });
 
         return view;
+    }
+
+    @SuppressLint("StaticFieldLeak")
+    private class FetchRecipesTask extends AsyncTask<Void, Void, Void> {
+
+        private NetworkModule networkModule;
+
+        FetchRecipesTask(NetworkModule networkModule) {
+            this.networkModule = networkModule;
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            List<Recipe> recipes = NetworkModule.fetchRecipes();
+            networkModule.cacheData(recipes);
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+
+            List<String> recipeNames = new ArrayList<>();
+            List<Recipe> recipes = networkModule.getRecipes();
+            for (int i = 0; i < recipes.size(); i++) {
+                recipeNames.add(recipes.get(i).getName());
+            }
+
+            ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                    Objects.requireNonNull(getActivity()),
+                    android.R.layout.simple_list_item_1,
+                    android.R.id.text1,
+                    recipeNames
+            );
+
+            listView.setAdapter(adapter);
+        }
     }
 }
