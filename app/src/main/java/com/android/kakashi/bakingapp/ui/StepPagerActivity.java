a87c358f6bbbdf72bedd0d57aa01241aa797ddff
Sri <k3rn3l16@outlook.com>
2018-06-24 20:49:12 -0700

    Add up navigation to recipe activity from step pager activity

--- app/src/main/java/com/android/kakashi/bakingapp/ui/StepPagerActivity.java
+++ app/src/main/java/com/android/kakashi/bakingapp/ui/StepPagerActivity.java
@@ -9,9 +9,11 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 import com.android.kakashi.bakingapp.MockData;
 import com.android.kakashi.bakingapp.R;
+import com.android.kakashi.bakingapp.RecipeActivity;
 
 import java.util.List;
 
@@ -22,23 +24,29 @@ public class StepPagerActivity extends AppCompatActivity {
 
     @BindView(R.id.pager)
     ViewPager stepPager;
+    private int recipeIndex;
 
     private static final String EXTRA_STEP_INDEX = "com.android.kakashi.bakingapp.step_index";
+    private static final String EXTRA_RECIPE_INDEX = "com.android.kakashi.bakingapp.recipe_index";
 
-    public static Intent start(Context packageContext, int currentIndex) {
+    public static Intent start(Context packageContext, int currentIndex, int recipeIndex) {
         Intent intent = new Intent(packageContext, StepPagerActivity.class);
         intent.putExtra(EXTRA_STEP_INDEX, currentIndex);
+        intent.putExtra(EXTRA_RECIPE_INDEX, recipeIndex);
 
         return intent;
     }
 
+    @SuppressWarnings("ConstantConditions")
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_step_pager);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
         ButterKnife.bind(this);
 
         int currentIndex = getIntent().getIntExtra(EXTRA_STEP_INDEX, 0);
+        recipeIndex = getIntent().getIntExtra(EXTRA_RECIPE_INDEX, -1);
 
         final List<String> steps = MockData.getData();
         FragmentManager fragmentManager = getSupportFragmentManager();
@@ -57,5 +65,17 @@ public class StepPagerActivity extends AppCompatActivity {
         });
 
         stepPager.setCurrentItem(currentIndex);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // Respond to the action bar's Up/Home button
+            case android.R.id.home:
+                Intent intent = RecipeActivity.startActivity(this, recipeIndex);
+                startActivity(intent);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
