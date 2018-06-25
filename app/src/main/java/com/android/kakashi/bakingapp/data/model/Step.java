2439ed49d84a60d3a4647a83ed4653901053e963
Sri <k3rn3l16@outlook.com>
2018-06-24 22:29:10 -0700

    Make Step object parcelable

--- app/src/main/java/com/android/kakashi/bakingapp/data/model/Step.java
+++ app/src/main/java/com/android/kakashi/bakingapp/data/model/Step.java
@@ -2,11 +2,14 @@
 
 package com.android.kakashi.bakingapp.data.model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
 @SuppressWarnings("unused")
-public class Step {
+public class Step implements Parcelable {
 
     @SerializedName("id")
     @Expose
@@ -23,6 +26,40 @@ public class Step {
     @SerializedName("thumbnailURL")
     @Expose
     private String thumbnailURL;
+
+    private Step(Parcel in) {
+        id = in.readInt();
+        shortDescription = in.readString();
+        description = in.readString();
+        videoURL = in.readString();
+        thumbnailURL = in.readString();
+    }
+
+    public static final Creator<Step> CREATOR = new Creator<Step>() {
+        @Override
+        public Step createFromParcel(Parcel in) {
+            return new Step(in);
+        }
+
+        @Override
+        public Step[] newArray(int size) {
+            return new Step[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(id);
+        dest.writeString(shortDescription);
+        dest.writeString(description);
+        dest.writeString(videoURL);
+        dest.writeString(thumbnailURL);
+    }
 
     public int getId() {
         return id;
