3c9f391976d2ee04348e6c3b5019fc177c66e813
Sri Nath <sri.developer731@gmail.com>
2018-09-21 17:45:43 -0400

    Add recipe image to layout redesign + new landscape layout design

--- app/src/main/java/com/android/kakashi/bakingapp/ui/adapter/RecipeAdapter.java
+++ app/src/main/java/com/android/kakashi/bakingapp/ui/adapter/RecipeAdapter.java
@@ -1,21 +1,21 @@
 package com.android.kakashi.bakingapp.ui.adapter;
 
 import android.content.Context;
-import android.graphics.Color;
+import android.content.res.Configuration;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.android.kakashi.bakingapp.R;
 import com.android.kakashi.bakingapp.data.model.Recipe;
+import com.android.kakashi.bakingapp.data.model.Step;
+import com.bumptech.glide.Glide;
 
 import java.util.List;
-
-import butterknife.BindView;
-import butterknife.ButterKnife;
 
 public class RecipeAdapter extends RecyclerView.Adapter<RecipeAdapter.RecipeHolder> {
 
@@ -42,7 +42,9 @@ public class RecipeAdapter extends RecyclerView.Adapter<RecipeAdapter.RecipeHold
 	@Override
 	public void onBindViewHolder(@NonNull RecipeHolder holder, int position) {
 		Recipe recipe = recipes.get(position);
-		holder.bind(recipe.getName(), recipe.getServings());
+		List<Step> steps = recipe.getSteps();
+		String recipeThumbnail = steps.get(steps.size() - 1).getVideoURL();
+		holder.bind(recipe.getName(), recipe.getServings(), recipeThumbnail);
 	}
 
 	@Override
@@ -60,17 +62,34 @@ public class RecipeAdapter extends RecyclerView.Adapter<RecipeAdapter.RecipeHold
 
 		TextView recipeNameTextView;
 		TextView recipeServingSize;
+		ImageView recipeImage;
+		TextView recipeServingsInfo;
+		boolean isInLandscapeMode;
 
 		public RecipeHolder(View itemView) {
 			super(itemView);
+
 			recipeNameTextView = itemView.findViewById(R.id.recipe_title);
 			recipeServingSize = itemView.findViewById(R.id.recipe_servings);
+			recipeImage = itemView.findViewById(R.id.recipe_image);
+
+			if (context.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+				isInLandscapeMode = true;
+				recipeServingsInfo = itemView.findViewById(R.id.recipe_servings_info);
+			}
 		}
 
-		void bind(String name, int servingSize) {
+		void bind(String name, int servingSize, String videoUrl) {
 			recipeNameTextView.setText(name);
-			recipeServingSize.setTextColor(Color.BLACK);
-			recipeServingSize.setText(String.valueOf(servingSize));
+			String servingsSizeAsText = String.valueOf(servingSize);
+			recipeServingSize.setText(servingsSizeAsText);
+			Glide.with(context)
+					.load(videoUrl)
+					.into(recipeImage);
+
+			if (isInLandscapeMode) {
+				recipeServingsInfo.setText(String.format("Serves %s", servingsSizeAsText));
+			}
 		}
 	}
 }
